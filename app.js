80f4c693b802df44a44816e6f6b40c412749c45a
Sudaraka Pieris <spie2381@uni.sydney.edu.au>
2025-06-06 01:50:26 +1000

    rolled back to the original app.js code

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sql = require('mssql'); // NEW
-require('dotenv').config(); // optional, for local development
 
 var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
@@ -24,40 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// ✅ NEW: Test DB connection route
-// app.get('/test-db', async (req, res) => {
-//   const config = {
-//     server: process.env.AZURE_SQL_SERVER,
-//     port: parseInt(process.env.AZURE_SQL_PORT),
-//     database: process.env.AZURE_SQL_DATABASE,
-//     user: process.env.AZURE_SQL_USERNAME,
-//     password: process.env.AZURE_SQL_PASSWORD,
-//     options: {
-//       encrypt: true,
-//       trustServerCertificate: false
-//     }
-//   };
-//
-//   try {
-//     await sql.connect(config);
-//
-//     // Query the current database name and return a sample table
-//     const dbNameResult = await sql.query`SELECT DB_NAME() AS CurrentDatabase`;
-//     const sampleDataResult = await sql.query`SELECT TOP 5 * FROM YourTableName`; // change this to a real table name
-//
-//     res.json({
-//       connectedDatabase: dbNameResult.recordset[0].CurrentDatabase,
-//       sampleData: sampleDataResult.recordset
-//     });
-//   } catch (err) {
-//     console.error('DB query error:', err);
-//     res.status(500).json({ error: err.message });
-//   } finally {
-//     sql.close();
-//   }
-// });
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -65,8 +29,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
